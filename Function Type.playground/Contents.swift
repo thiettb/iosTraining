0ee5e731d00e022dd7ce724be3530fd2978800df
thiettb <thietb.hust@gmail.com>
2018-02-27 09:35:31 +0700

    Function type

--- Function Type.playground/Contents.swift
+++ Function Type.playground/Contents.swift
@@ -2,4 +2,165 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//MARK:: Defining and Calling Functions
+func greet(person: String) -> String {
+    let greeting = "Hello, " + person + "!"
+    return greeting
+}
+print(greet(person: "Anna"))
+
+// rút gọn trong phần body
+func greetAgain(person: String) -> String {
+    return "Hello again, " + person + "!"
+}
+print(greetAgain(person: "Anna"))
+
+// MARK::Function Parameters and Return Values
+// Functions Without Parameters
+func sayHelloWorld() -> String {
+    return "hello, world"
+}
+print(sayHelloWorld())
+
+// Functions With Multiple Parameters
+func greet(person: String, alreadyGreeted: Bool) -> String {
+    if alreadyGreeted {
+        return greetAgain(person: person)
+    } else {
+        return greet(person: person)
+    }
+}
+print(greet(person: "Tim", alreadyGreeted: true))
+
+//Functions Without Return Values
+func greetT(person: String) {
+    print("Hello, \(person)!")
+}
+greetT(person: "Dave")
+
+//Functions with Multiple Return Values
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+let bounds = minMax(array: [8, -6, 2, 109, 3, 71])
+print("min is \(bounds.min) and max is \(bounds.max)")
+
+//Optional Tuple Return Types
+func minMax2(array: [Int]) -> (min: Int, max: Int)? {
+    if array.isEmpty { return nil }
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+if let bounds = minMax2(array: [8, -6, 2, 109, 3, 71]) {
+    print("min is \(bounds.min) and max is \(bounds.max)")
+}
+//MARK:: Function Argument Labels and Parameter Names
+//Specifying Argument Labels
+func someFunction(argumentLabel parameterName: Int) {
+    
+}
+func greet3(person: String, from hometown: String) -> String {
+    return "Hello \(person)!  Glad you could visit from \(hometown)."
+}
+print(greet3(person: "Bill", from: "Cupertino"))
+
+// Omitting Argument Labels
+func someFunction(_ firstParameterName: Int, secondParameterName: Int) {
+    
+}
+someFunction(1, secondParameterName: 2)
+
+// Default Parameter Values
+func someFunction(parameterWithoutDefault: Int, parameterWithDefault: Int = 12) {
+}
+someFunction(parameterWithoutDefault: 3, parameterWithDefault: 6) // parameterWithDefault is 6
+someFunction(parameterWithoutDefault: 4) // parameterWithDefault is 12
+
+// Variadic Parameters
+func arithmeticMean(_ numbers: Double...) -> Double {
+    var total: Double = 0
+    for number in numbers {
+        total += number
+    }
+    return total / Double(numbers.count)
+}
+arithmeticMean(1, 2, 3, 4, 5)
+// returns 3.0, which is the arithmetic mean of these five numbers
+arithmeticMean(3, 8.25, 18.75)
+// returns 10.0, which is the arithmetic mean of these three numbers
+
+// In-Out Parameters
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+//MARK:: Function Types
+func addTwoInts(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+func multiplyTwoInts(_ a: Int, _ b: Int) -> Int {
+    return a * b
+}
+
+//Using Function Types
+var mathFunction: (Int, Int) -> Int = addTwoInts
+print("Result: \(mathFunction(2, 3))")
+
+mathFunction = multiplyTwoInts
+print("Result: \(mathFunction(2, 3))")
+
+// Function Types as Parameter Types
+func printMathResult(_ mathFunction: (Int, Int) -> Int, _ a: Int, _ b: Int) {
+    print("Result: \(mathFunction(a, b))")
+}
+printMathResult(addTwoInts, 3, 5)
+
+//Function Types as Return Types
+func stepForward(_ input: Int) -> Int {
+    return input + 1
+}
+func stepBackward(_ input: Int) -> Int {
+    return input - 1
+}
+func chooseStepFunction(backward: Bool) -> (Int) -> Int {
+    return backward ? stepBackward : stepForward
+}
+var currentValue = 3
+let moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
+while currentValue != 0 {
+    print("\(currentValue)... ")
+    currentValue = moveNearerToZero(currentValue)
+}
+
+//Nested Functions
+func chooseStepFunction2(backward: Bool) -> (Int) -> Int {
+    func stepForward(input: Int) -> Int { return input + 1 }
+    func stepBackward(input: Int) -> Int { return input - 1 }
+    return backward ? stepBackward : stepForward
+}
+var currentValue2 = -4
+let moveNearerToZero2 = chooseStepFunction2(backward: currentValue2 > 0)
+// moveNearerToZero now refers to the nested stepForward() function
+while currentValue2 != 0 {
+    print("\(currentValue2)... ")
+    currentValue2 = moveNearerToZero2(currentValue2)
+}
